efc75fdb79fd48b2a021f00fa4bb4fcd5b56421c
Sagi Shadur <saroad2@gmail.com>
2021-06-10 21:26:41 +0300

    First implementation of learner

--- src/ball_sort_solver/__main__.py
+++ src/ball_sort_solver/__main__.py
@@ -4,6 +4,8 @@ from pathlib import Path
 import click
 
 from ball_sort_solver.ball_sort_game import BallSortGame
+from ball_sort_solver.ball_sort_learner import BallSortLearner
+from ball_sort_solver.ball_sort_state_getter import BallSortStateGetter
 from ball_sort_solver.exceptions import IllegalMove
 
 
@@ -37,6 +39,39 @@ def play_ball_sort(configuration):
         except IllegalMove as e:
             click.echo(f"You tried to make an illegal move: {e}")
     click.echo("Game won!")
+
+
+@ball_sort_solver_cli.command("train")
+@click.option(
+    "-c", "--configuration",
+    type=click.Path(dir_okay=False, exists=True),
+)
+def train_ball_sort(configuration):
+    configuration = (
+        Path(configuration)
+        if configuration is not None
+        else Path.cwd() / "configuration.json"
+    )
+    with open(configuration, mode="r") as fd:
+        config_dict = json.load(fd)
+    game = BallSortGame(**config_dict["game"])
+    state_getter = BallSortStateGetter()
+    learner = BallSortLearner(
+        game=game,
+        state_getter=state_getter,
+        **config_dict["learner"]
+    )
+    train_config = config_dict["train"]
+    episodes = train_config["episodes"]
+    with click.progressbar(length=episodes, show_pos=True, show_percent=False) as bar:
+        for _ in bar:
+            learner.run_episode()
+            history = learner.last_train_history
+            bar.label = (
+                f"Rewards: {history['reward']:.2f}, "
+                f"Duration: {history['duration']}, "
+                f"Score: {history['score']}"
+            )
 
 
 if __name__ == '__main__':
---
+++ src/ball_sort_solver/ball_sort_learner.py
@@ -0,0 +1,262 @@
+import numpy as np
+import tensorflow as tf
+
+from ball_sort_solver.ball_sort_game import BallSortGame
+from ball_sort_solver.ball_sort_state_getter import BallSortStateGetter
+from ball_sort_solver.exceptions import IllegalMove
+
+
+class BallSortLearner:
+
+    def __init__(
+        self,
+        game: BallSortGame,
+        state_getter: BallSortStateGetter,
+        gamma: float,
+        tau: float,
+        noise_std: float,
+        won_reward: float,
+        score_reward: float,
+        illegal_move_loss: float,
+        move_loss: float,
+        buffer_capacity: int,
+        batch_size: int,
+        actor_learning_rate: int,
+        critic_learning_rate: int,
+        actor_inner_layer_neurons: int,
+        critic_inner_layer_neurons: int,
+    ):
+        self.game = game
+        self.state_getter = state_getter
+
+        self.gamma = gamma
+        self.tau = tau
+        self.noise_std = noise_std
+
+        self.won_reward = won_reward
+        self.score_reward = score_reward
+        self.illegal_move_loss = illegal_move_loss
+        self.move_loss = move_loss
+
+        self.actor = self.create_actor_model(actor_inner_layer_neurons)
+        self.critic = self.create_critic_model(critic_inner_layer_neurons)
+
+        self.actor_optimizer = tf.keras.optimizers.Adam(actor_learning_rate)
+        self.critic_optimizer = tf.keras.optimizers.Adam(critic_learning_rate)
+
+        self.target_actor = self.create_actor_model(actor_inner_layer_neurons)
+        self.target_critic = self.create_critic_model(critic_inner_layer_neurons)
+
+        self.target_actor.set_weights(self.actor.get_weights())
+        self.target_critic.set_weights(self.critic.get_weights())
+
+        # Number of "experiences" to store at max
+        self.buffer_capacity = buffer_capacity
+        # Num of tuples to train on.
+        self.batch_size = batch_size
+
+        # Its tells us num of times record() was called.
+        self.buffer_counter = 0
+
+        # Instead of list of tuples as the exp.replay concept go
+        # We use different np.arrays for each tuple element
+        self.state_buffer = np.zeros((self.buffer_capacity, self.state_size))
+        self.action_buffer = np.zeros((self.buffer_capacity, self.actions_size))
+        self.reward_buffer = np.zeros((self.buffer_capacity, 1))
+        self.next_state_buffer = np.zeros((self.buffer_capacity, self.state_size))
+
+        self.train_history = []
+
+    @property
+    def state_size(self):
+        return self.state_getter.size(self.game)
+
+    @property
+    def actions_size(self):
+        return self.game.stacks_number
+
+    @property
+    def last_train_history(self):
+        if len(self.train_history) == 0:
+            return None
+        return self.train_history[-1]
+
+    @tf.function
+    def update(
+        self, state_batch, action_batch, reward_batch, next_state_batch,
+    ):
+        # Training and updating Actor & Critic networks.
+        # See Pseudo Code.
+        with tf.GradientTape() as tape:
+            target_actions = self.target_actor(next_state_batch, training=True)
+            y = reward_batch + self.gamma * self.target_critic(
+                [next_state_batch, target_actions], training=True
+            )
+            critic_value = self.critic([state_batch, action_batch], training=True)
+            critic_loss = tf.math.reduce_mean(tf.math.square(y - critic_value))
+
+        critic_grad = tape.gradient(critic_loss, self.critic.trainable_variables)
+        self.critic_optimizer.apply_gradients(
+            zip(critic_grad, self.critic.trainable_variables)
+        )
+
+        with tf.GradientTape() as tape:
+            actions = self.actor(state_batch, training=True)
+            critic_value = self.critic([state_batch, actions], training=True)
+            # Used `-value` as we want to maximize the value given
+            # by the critic for our actions
+            actor_loss = -tf.math.reduce_mean(critic_value)
+
+        actor_grad = tape.gradient(actor_loss, self.actor.trainable_variables)
+        self.actor_optimizer.apply_gradients(
+            zip(actor_grad, self.actor.trainable_variables)
+        )
+
+    def run_episode(self):
+        self.game.reset()
+        episodic_reward = 0
+        count = 0
+
+        while True:
+            count += 1
+
+            prev_state, action, reward, current_state, done = self.make_move()
+
+            self.record(prev_state, action, reward, current_state)
+            episodic_reward += reward
+
+            self.learn()
+            self.update_model(model=self.actor, target_model=self.target_actor)
+            self.update_model(model=self.critic, target_model=self.target_critic)
+
+            # End this episode when `done` is True
+            if done:
+                break
+
+        self.save_history(
+            reward=episodic_reward,
+            duration=count,
+            score=self.game.score
+        )
+
+    def policy(self, state):
+        sampled_actions = tf.squeeze(self.actor(state.reshape(-1, *state.shape)))
+        noise = np.random.normal(scale=self.noise_std, size=(self.actions_size,))
+
+        return sampled_actions.numpy() + noise
+
+    def make_move(self):
+        prev_state = self.state_getter.get_state(self.game)
+        action = self.policy(prev_state)
+        prev_score = self.game.score
+
+        from_index, to_index = np.argmin(action), np.argmax(action)
+        try:
+            self.game.move(from_index=from_index, to_index=to_index)
+        except IllegalMove:
+            reward = -self.illegal_move_loss
+            return prev_state, action, reward, prev_state, True
+        current_state = self.state_getter.get_state(self.game)
+        if self.game.won:
+            done = True
+            reward = self.won_reward
+        else:
+            done = False
+            current_score = self.game.score
+            reward = (
+                self.score_reward
+                if current_score > prev_score
+                else -self.move_loss
+            )
+        return prev_state, action, reward, current_state, done
+
+    def update_model(self, model, target_model):
+        model_weights, target_weights = target_model.get_weights(), model.get_weights()
+        new_weights = []
+        for (a, b) in zip(model_weights, target_weights):
+            new_weights.append(a * self.tau + b * (1 - self.tau))
+        target_model.set_weights(new_weights)
+
+    def record(self, prev_state, action, reward, current_state):
+        # Set index to zero if buffer_capacity is exceeded,
+        # replacing old records
+
+        self.state_buffer[self.buffer_counter] = prev_state
+        self.action_buffer[self.buffer_counter] = action
+        self.reward_buffer[self.buffer_counter] = reward
+        self.next_state_buffer[self.buffer_counter] = current_state
+
+        self.buffer_counter += 1
+        self.buffer_counter %= self.buffer_capacity
+
+    def learn(self):
+        # Get sampling range
+        record_range = min(self.buffer_counter, self.buffer_capacity)
+        # Randomly sample indices
+        batch_indices = np.random.choice(record_range, self.batch_size)
+
+        # Convert to tensors
+        state_batch = tf.convert_to_tensor(self.state_buffer[batch_indices])
+        action_batch = tf.convert_to_tensor(self.action_buffer[batch_indices])
+        reward_batch = tf.convert_to_tensor(self.reward_buffer[batch_indices])
+        reward_batch = tf.cast(reward_batch, dtype=tf.float32)
+        next_state_batch = tf.convert_to_tensor(self.next_state_buffer[batch_indices])
+
+        self.update(state_batch, action_batch, reward_batch, next_state_batch)
+
+    def save_history(self, **kwargs):
+        self.train_history.append(kwargs)
+
+    def create_actor_model(
+        self,
+        actor_inner_layer_neurons,
+    ):
+        return tf.keras.models.Sequential(
+            [
+                tf.keras.layers.Input(shape=self.state_size),
+                tf.keras.layers.Dense(
+                    actor_inner_layer_neurons,
+                    activation="relu",
+                    kernel_initializer=tf.keras.initializers.he_uniform,
+                ),
+                tf.keras.layers.Dense(
+                    actor_inner_layer_neurons,
+                    activation="relu",
+                    kernel_initializer=tf.keras.initializers.he_uniform
+                ),
+                tf.keras.layers.Dense(
+                    self.actions_size,
+                    activation="tanh",
+                    kernel_initializer=tf.keras.initializers.he_uniform)
+            ]
+        )
+
+    def create_critic_model(
+        self,
+        critic_inner_layer_neurons,
+    ):
+        state_input = tf.keras.layers.Input(shape=(self.state_size,))
+        state_out = tf.keras.layers.Dense(16, activation="relu")(state_input)
+        state_out = tf.keras.layers.Dense(32, activation="relu")(state_out)
+
+        # Action as input
+        action_input = tf.keras.layers.Input(shape=(self.actions_size,))
+        action_out = tf.keras.layers.Dense(32, activation="relu")(action_input)
+
+        # Both are passed through seperate layer before concatenating
+        concat = tf.keras.layers.Concatenate()([state_out, action_out])
+
+        out = tf.keras.layers.Dense(
+            critic_inner_layer_neurons,
+            activation="relu",
+        )(concat)
+        out = tf.keras.layers.Dense(
+            critic_inner_layer_neurons,
+            activation="relu",
+        )(out)
+        outputs = tf.keras.layers.Dense(1)(out)
+
+        # Outputs single value for give state-action
+        model = tf.keras.Model([state_input, action_input], outputs)
+
+        return model
